e6a0727d5a5fd24b13d6508271067b63b2079e91
Álvaro Valencia <avalenciaparra@gmail.com>
2018-11-29 09:02:48 +0100

    included a test dataset in the "datasets/" directory. Included concise examples at Main.scala

--- src/main/scala/es/us/idea/adt/Main.scala
+++ src/main/scala/es/us/idea/adt/Main.scala
@@ -1,16 +1,12 @@
 package es.us.idea.adt
 
-import es.us.idea.adt.data._
-import es.us.idea.adt.data.functions.ADTReductionFunction
-import org.apache.spark.sql.{Row, SQLContext, SparkSession}
-import org.apache.spark.sql.functions._
+import es.us.idea.adt.data.functions.{ADTDataFunction, ADTReductionFunction}
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.DataTypes
 import org.joda.time.{Days, LocalDate}
 
 object Main {
   def main(args: Array[String]) = {
-
-    //println("Hello World!")
 
     val spark = SparkSession.builder()
       .master("local[*]")
@@ -21,6 +17,14 @@ object Main {
     import es.us.idea.adt.dsl.implicits._
     import functions._
 
+    val translations = Seq("TA", "TB", "TC", "TD", "TE", "TF", "TG", "TH", "TI", "TJ", "TK", "TL", "TM").zipWithIndex.toMap
+
+    // ADT User Defined Function to translate the "tariff" field
+    val translate = ADTDataFunction((s: String) => {
+      translations.getOrElse(s, -1)
+    } , DataTypes.IntegerType)
+
+    // ADT User Defined Function to calculate the number of days between the "startDate" and "endDate"
     val daysBetweenDates = ADTReductionFunction((s: Seq[Some[java.sql.Date]]) => {
       (s.headOption, s.lastOption) match {
         case (Some(endOpt), Some(startOpt)) => (endOpt, startOpt) match {
@@ -31,25 +35,27 @@ object Main {
       }
     } , DataTypes.IntegerType)
 
-    val ds = spark.read.json(s"/home/alvaro/datasets/hidrocantabrico_split.json")
+    // Read the Dataset and apply the Data Transformation Functions
+    val ds = spark.read.json("datasets/power_consumption.json")
       .adt(
-        d"consumoMinFormat" * ("consumo" &+ d"minPot" < min("potencias.p1", "potencias.p2", "potencias.p3")),
-        d"tariff" < "tarifa",
-        d"PC_MAX" * (max("potenciaContratada.p1", "potenciaContratada.p4"), max("potenciaContratada.p2", "potenciaContratada.p5"), max("potenciaContratada.p3", "potenciaContratada.p4")),
-        d"consumoFormateado" * ("consumo" &+ (
-          (1 to 6).map(i => d(s"pot$i") < s"potencias.p$i"): _*
-        )),
-        d"C_MAX" * ("consumo" &* (
-          (1 to 3).map(i => max(s"potencias.p$i", s"potencias.p${i+3}")) : _*
-        )),
-        d"avgConsumoPot" + (
-          (1 to 3).map(i => d(s"p$i") < (avg("consumo" & max(s"potencias.p$i", s"potencias.p${i+3}")) / times(10000) / asInt)) : _*
+        d"ID" < "customerID", //T1
+        d"T" < "tariff" / translate, //T2
+        d"CP" * ( //T3
+          max("contractedPower.period1", "contractedPower.period4") / asInt,
+          max("contractedPower.period2", "contractedPower.period5") / asInt,
+          max("contractedPower.period3", "contractedPower.period6") / asInt
         ),
-        d"billingDays" * ("consumo" & "diasFacturacion"),
-        d"consumoDate" * ("consumo" & ("fechaFinLectura" / asDate("dd/MM/yyyy"))),
-        d"calculatedBillingDays" * ("consumo" & reduce("fechaFinLectura" / asDate("dd/MM/yyyy"), "fechaInicioLectura" / asDate("dd/MM/yyyy"))(daysBetweenDates))
+        d"C" * ("consumption" &* ( //T4
+          (1 to 3).map(i => max(s"power.period$i", s"power.period${i+3}")) : _*
+          )),
+        d"AVG_C" + ( //T5
+          (1 to 3).map(i => d(s"p$i") < avg("consumption" & max(s"power.period$i", s"power.period${i+3}"))) : _*
+          ),
+        d"BD" * ("consumption" & reduce("endDate" / asDate("dd/MM/yyyy"), "startDate" / asDate("dd/MM/yyyy"))(daysBetweenDates)) //T6
       )
+      .select("ID", "T", "CP", "C", "AVG_C", "BD")
 
+    // Show a preview of the Dataset and print its schema
     ds.show(false)
     ds.printSchema()
 
